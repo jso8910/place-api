963636af8188a4510a3d95aa3bb6e744b353b519
jso8910 <mail4jasonr@gmail.com>
2022-04-18 00:26:42 +0800

    Ran prettier and added uid to search

--- routes/pixels.js
+++ routes/pixels.js
@@ -1,69 +1,71 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Pixel = require('../utils/database')
+const Pixel = require("../utils/database");
 const { Op } = require("sequelize");
 
-router.get('/remaining', async (req, res, next) => {
+router.get("/remaining", async (req, res, next) => {
   if (req.query.uid === undefined) {
     res.status(400);
-    res.json({status: 'error', message: 'Missing URL query uid'});
+    res.json({ status: "error", message: "Missing URL query uid" });
     return;
   }
   const pixels = await Pixel.findAll({
     where: {
       user_id: req.query.uid,
       timestamp: {
-          [Op.lt]: new Date('2022-04-04 22:47:40.185000 UTC')
-      }
-    }
+        [Op.lt]: new Date("2022-04-04 22:47:40.185000 UTC"),
+      },
+    },
   });
-  const pixel_on_coords = await Promise.all(pixels.map(pixel => {
-    return Pixel.findAll({
-      where: {
-        x: pixel.x,
-        y: pixel.y,
-        timestamp: {
-          [Op.lt]: new Date('2022-04-04 22:47:40.185000 UTC')
-        }
-      }
-    });
-  }));
+  const pixel_on_coords = await Promise.all(
+    pixels.map((pixel) => {
+      return Pixel.findAll({
+        where: {
+          x: pixel.x,
+          y: pixel.y,
+          timestamp: {
+            [Op.lt]: new Date("2022-04-04 22:47:40.185000 UTC"),
+          },
+        },
+      });
+    })
+  );
   const remaining = pixels.filter((pixel, index) => {
     // I don't wanna deal with rectangles
     if (pixel.x2 !== null) {
       return false;
     }
     const pixel_on_coord = pixel_on_coords[index];
-    const remaining = pixel_on_coord.map(n_pixel => {
+    const remaining = pixel_on_coord.map((n_pixel) => {
       if (n_pixel.timestamp > pixel.timestamp) {
         return false;
       }
       return true;
-    })
-    return remaining.every(val => val === true);
+    });
+    return remaining.every((val) => val === true);
   });
   res.json({
-    status: 'ok',
+    status: "ok",
     count: remaining.length,
-    pixels: remaining
+    pixels: remaining,
   });
 });
 
-router.get('/all', async (req, res, next) => {
+router.get("/all", async (req, res, next) => {
   if (req.query.uid === undefined) {
     res.status(400);
-    res.json({status: 'error', message: 'Missing URL query uid'});
+    res.json({ status: "error", message: "Missing URL query uid" });
     return;
   }
   const pixels = await Pixel.findAll({
     where: {
-      user_id: req.query.uid
-    }
+      user_id: req.query.uid,
+    },
   });
   res.json({
-    status: 'ok',
+    status: "ok",
     count: pixels.length,
-    pixels: pixels
+    pixels: pixels,
   });
 });
 
--- routes/search.js
+++ routes/search.js
@@ -1,45 +1,54 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Pixel = require('../utils/database')
+const Pixel = require("../utils/database");
 const { Op } = require("sequelize");
 
-router.get('/', async (req, res, next) => {
-    // I'm thinking that, even if the user defines just one pixel, the client will have x2 and y2 be equal to x1 and y1
-    const params = ['t1', 't2', 'x1', 'y1', 'x2', 'y2'];
-    if (!params.every(param => Object.keys(req.query).includes(param))) {
-        res.status(400);
-        res.json({status: 'error', message: 'Missing URL query uid'});
-        return;
-    }
-
-    // t1 and t2 are ISO8601 strings
-    const dt1 = new Date(req.query.t1)
-    const dt2 = new Date(req.query.t2)
-    if (!([dt1, dt2].every(date => (new Date(date) !== "Invalid Date") && !isNaN(new Date(date))))) {
-        res.status(400);
-        res.json({status: 'error', message: 'Timestamp invalid'});
-        return;
-    }
-    const pixels = await Pixel.findAll({
-        where: {
-            timestamp: {
-                [Op.between]: [dt1, dt2]
-            },
-            x: {
-                [Op.between]: [req.query.x1, req.query.x2]
-            },
-            y: {
-                [Op.between]: [req.query.y1, req.query.y2]
-            },
-            x2: null,
-            y2: null
-        },
-    })
+router.get("/", async (req, res, next) => {
+  // I'm thinking that, even if the user defines just one pixel, the client will have x2 and y2 be equal to x1 and y1
+  const params = ["t1", "t2", "x1", "y1", "x2", "y2"];
+  if (!params.every((param) => Object.keys(req.query).includes(param))) {
+    res.status(400);
     res.json({
-        status: 'ok',
-        count: pixels.length,
-        pixels: pixels
+      status: "error",
+      message: "Incomplete request. All parameters must be sent.",
     });
+    return;
+  }
+
+  // t1 and t2 are ISO8601 strings
+  const dt1 = new Date(req.query.t1);
+  const dt2 = new Date(req.query.t2);
+  if (
+    ![req.query.t1, req.query.t2].every(
+      (date) => new Date(date) !== "Invalid Date" && !isNaN(new Date(date))
+    )
+  ) {
+    res.status(400);
+    res.json({ status: "error", message: "Timestamp invalid" });
+    return;
+  }
+  const uid = req.query.uid ? { user_id: req.query.uid } : {};
+  const pixels = await Pixel.findAll({
+    where: {
+      timestamp: {
+        [Op.between]: [dt1, dt2],
+      },
+      x: {
+        [Op.between]: [req.query.x1, req.query.x2],
+      },
+      y: {
+        [Op.between]: [req.query.y1, req.query.y2],
+      },
+      x2: null,
+      y2: null,
+      ...uid,
+    },
+  });
+  res.json({
+    status: "ok",
+    count: pixels.length,
+    pixels: pixels,
+  });
 });
 
 module.exports = router;
